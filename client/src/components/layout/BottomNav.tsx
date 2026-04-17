10e428358480d0698c3bb1cbaf4afbcc06b47a6c
robinkasanalenovo-commits <robin.kasana.lenovo@gmail.com>
2026-04-18 01:16:55 +0530

    Update BottomNav.tsx

--- client/src/components/layout/BottomNav.tsx
+++ client/src/components/layout/BottomNav.tsx
@@ -1,3 +1,4 @@
+import { Link } from "wouter";
 import { Home, Grid, ShoppingCart, User } from "lucide-react";
 
 interface BottomNavProps {
@@ -5,30 +6,36 @@ interface BottomNavProps {
   onHomeClick?: () => void;
 }
 
-export function BottomNav({ onCartClick, onHomeClick }: BottomNavProps) {
+export function BottomNav({ onCartClick }: BottomNavProps) {
   return (
     <div className="fixed bottom-0 left-0 right-0 z-50 border-t border-gray-200 bg-white shadow">
       <div className="flex h-16 items-center justify-around">
-        
-        <button className="flex flex-col items-center text-gray-600">
-          <Home size={20} />
-          <span className="text-xs">Home</span>
-        </button>
 
-        <button className="flex flex-col items-center text-gray-600">
-          <Grid size={20} />
-          <span className="text-xs">Categories</span>
-        </button>
+        <Link href="/">
+          <div className="flex flex-col items-center text-gray-600 cursor-pointer">
+            <Home size={20} />
+            <span className="text-xs">Home</span>
+          </div>
+        </Link>
+
+        <Link href="/categories">
+          <div className="flex flex-col items-center text-gray-600 cursor-pointer">
+            <Grid size={20} />
+            <span className="text-xs">Categories</span>
+          </div>
+        </Link>
 
         <button onClick={onCartClick} className="flex flex-col items-center text-gray-600">
           <ShoppingCart size={20} />
           <span className="text-xs">Cart</span>
         </button>
 
-        <button className="flex flex-col items-center text-gray-600">
-          <User size={20} />
-          <span className="text-xs">Profile</span>
-        </button>
+        <Link href="/profile">
+          <div className="flex flex-col items-center text-gray-600 cursor-pointer">
+            <User size={20} />
+            <span className="text-xs">Profile</span>
+          </div>
+        </Link>
 
       </div>
     </div>
