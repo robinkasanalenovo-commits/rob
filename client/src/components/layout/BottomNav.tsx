a462fccd632cd432bbf48238980f857269da3c86
robinkasanalenovo-commits <robin.kasana.lenovo@gmail.com>
2026-04-18 01:22:36 +0530

    Update BottomNav.tsx

--- client/src/components/layout/BottomNav.tsx
+++ client/src/components/layout/BottomNav.tsx
@@ -1,4 +1,3 @@
-import { Link } from "wouter";
 import { Home, Grid, ShoppingCart, User } from "lucide-react";
 
 interface BottomNavProps {
@@ -6,37 +5,29 @@ interface BottomNavProps {
   onHomeClick?: () => void;
 }
 
-export function BottomNav({ onCartClick }: BottomNavProps) {
+export function BottomNav({ onCartClick, onHomeClick }: BottomNavProps) {
   return (
     <div className="fixed bottom-0 left-0 right-0 z-50 border-t border-gray-200 bg-white shadow">
       <div className="flex h-16 items-center justify-around">
+        <a href="/" className="flex flex-col items-center text-gray-600">
+          <Home size={20} />
+          <span className="text-xs">Home</span>
+        </a>
 
-        <Link href="/">
-          <div className="flex flex-col items-center text-gray-600 cursor-pointer">
-            <Home size={20} />
-            <span className="text-xs">Home</span>
-          </div>
-        </Link>
-
-        <Link href="/categories">
-          <div className="flex flex-col items-center text-gray-600 cursor-pointer">
-            <Grid size={20} />
-            <span className="text-xs">Categories</span>
-          </div>
-        </Link>
+        <a href="/categories" className="flex flex-col items-center text-gray-600">
+          <Grid size={20} />
+          <span className="text-xs">Categories</span>
+        </a>
 
         <button onClick={onCartClick} className="flex flex-col items-center text-gray-600">
           <ShoppingCart size={20} />
           <span className="text-xs">Cart</span>
         </button>
 
-        <Link href="/profile">
-          <div className="flex flex-col items-center text-gray-600 cursor-pointer">
-            <User size={20} />
-            <span className="text-xs">Profile</span>
-          </div>
-        </Link>
-
+        <a href="/profile" className="flex flex-col items-center text-gray-600">
+          <User size={20} />
+          <span className="text-xs">Profile</span>
+        </a>
       </div>
     </div>
   );
